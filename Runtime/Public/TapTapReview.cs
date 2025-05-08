6ef6dc792809e0cb9699eecd87cee91e1b42332d
lc1006 <1176168734@qq.com>
2025-05-08 16:35:19 +0800

    feat:update upm

---
+++ Runtime/Public/TapTapReview.cs
@@ -0,0 +1,15 @@
+﻿using TapSDK.Review.Internal;
+
+namespace TapSDK.Review
+{
+    public class TapTapReview
+    {
+
+        public static readonly string Version = "4.6.1-beta.11";
+
+        public static void OpenReview()
+        {
+            TapTapReviewInner.OpenReview();
+        }
+    }
+}
